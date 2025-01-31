5865cdc673a2c068921f16d35ea263d624c2d9d4
Sorepan <s3rp4ntin4scomi4ando3nmibod4xd@gmail.com>
2025-01-30 22:20:24 -0700

    ortographic error

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitnoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'django_crud.urls'
